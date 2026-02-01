379dac487c766db016faab274da95ff066f38db1
Ozan Güngör <82978076+ozanggnr@users.noreply.github.com>
2026-02-01 04:51:48 +0300

    Fix API URL and backend bugs13

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@
 // 1. LOCAL (Testing):
 // const API_URL = "http://127.0.0.1:8000";
 // 2. PRODUCTION (Render/Railway):
-const API_URL = "https://wolfee-backend.onrender.com/";
+const API_URL = "https://wolfee-backend.onrender.com";
 // -------------------------------------------------------------------------
